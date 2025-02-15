e1e2681cb792b079eed2cf9b7337a70e65e75de7
nana <nkn262628@gmail.com>
2025-02-16 07:29:10 +0800

    租借連資料庫+分頁排序

--- server/api/index.js
+++ server/api/index.js
@@ -14,6 +14,8 @@ import cartRouter from "../routes/cart/index.js";
 import categoriesRouter from "../routes/categories/index.js";
 import brandRouter from "../routes/brands/index.js";
 import activityRouter from "../routes/activity/index.js";
+import rentRouter from "../routes/rent/index.js";
+import rentDetailRouter from "../routes/rent/detail.js";
 
 // 建立 Express 應用程式
 const app = express();
@@ -60,6 +62,10 @@ apiRouter.use("/brands", brandRouter); // 負責 `/api/brands`
 
 // 活動相關路由
 apiRouter.use("/activity", activityRouter);
+
+// 租借相關路由
+apiRouter.use("/rent", rentRouter); // 負責 `/api/rent`
+apiRouter.use("/rent", rentDetailRouter); // 負責 `/api/rent/:id`
 
 // 捕捉 404 錯誤
 app.use((req, res, next) => {
