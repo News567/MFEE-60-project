471d0e436a4f70df8ffc87839d006793e26dec8d
Catherine <catherine50733@gmail.com>
2025-02-18 13:52:11 +0800

    server part change "auth.js"

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,31 @@
 import jwt from "jsonwebtoken";
 
-export const authenticateUser = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; //Bearer token
+export const checkToken = (req, res, next) => {
+  let token = req.get("Authorization");
 
   if (!token) {
-    return res.status(401).json({ success: false, message: "未授權，請登入" });
+    return res.status(401).json({
+      status: "error",
+      message: "無驗證資料, 請重新登入",
+    });
   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // 把解析出的 `userId` 存到 `req.user` 後續可以從req.user.id拿到
-    // 因為decoded是這個middleware的參數 為了讓外部使用 這樣 req.user 變成 req 物件的一部分
-    next();
-  } catch (error) {
-    return res.status(401).json({ success: false, message: "無效的 Token" });
+  if (!token.startsWith("Bearer ")) {
+    return res.status(401).json({
+      status: "error",
+      message: "驗證資料錯誤, 請重新登入",
+    });
   }
+
+  token = token.slice(7); // 去掉 "Bearer " 前綴
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        status: "error",
+        message: "驗證資料失效, 請重新登入",
+      });
+    }
+    req.decoded = decoded; // 使用 `req.decoded`
+    next();
+  });
 };
