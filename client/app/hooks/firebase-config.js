c708ee5aa23a2db55471aa80fda7342628540427
Catherine <catherine50733@gmail.com>
2025-03-04 13:30:23 +0800

    .

---
+++ client/app/hooks/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6CfKvsALmkxmAhM6oiPW1luDBp0QwuQA",
+    authDomain: "react--login-69325.firebaseapp.com",
+    projectId: "react--login-69325",
+    storageBucket: "react--login-69325.firebasestorage.app",
+    messagingSenderId: "941435651524",
+    appId: "1:941435651524:web:1b7993ce6e9bca82a6d091"
+  };
+  
+  const app = initializeApp(firebaseConfig);
